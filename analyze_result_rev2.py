1c7c4f231358aed28dcd1de70bd395c51bb813ea
metab0t <metab0t@outlook.com>
2024-11-29 23:01:28 +0800

    Use newest POI syntax

--- analyze_result_rev2.py
+++ analyze_result_rev2.py
@@ -9,10 +9,12 @@ def extract_timing(f):
     total_time = None
     ipopt_time = None
     ad_time = None
+    number_of_iterations = None
+    objective_value = None
     with open(f, "r") as f:
         lines = f.readlines()
     for line in reversed(lines):
-        if total_time and ipopt_time and ad_time:
+        if total_time and ipopt_time and ad_time and number_of_iterations and objective_value:
             break
         if "OverallAlgorithm" in line:
             ipopt_time = float(line.split()[1])
@@ -20,7 +22,11 @@ def extract_timing(f):
             ad_time = float(line.split()[2])
         elif "Total time" in line:
             total_time = float(line.split()[2])
-    return total_time, ipopt_time, ad_time
+        elif "Number of Iterations....:" in line:
+            number_of_iterations = int(line.split()[3])
+        elif "Objective...............:" in line:
+            objective_value = float(line.split()[2])
+    return total_time, ipopt_time, ad_time, number_of_iterations, objective_value
 
 
 def main():
@@ -41,14 +47,13 @@ def main():
         "examodels",
         "poi",
     ]
-    # solvers = ["examodels", "poi"]
 
     # construct a dataframe, case as row, solver as column
     df = pd.DataFrame(columns=solvers, index=test_cases)
 
     solvers_details = []
     for solver in solvers:
-        for i in range(3):
+        for i in range(2):
             solvers_details.append(f"{solver}.{i}")
     wide_df = pd.DataFrame(columns=solvers_details, index=test_cases)
 
@@ -61,7 +66,7 @@ def main():
         # print(f"Get {case} with {solver}")
         logpath = logdir / f"{case}_{suffix}.log"
         with open(logpath, "r") as f:
-            total_time, ipopt_time, ad_time = extract_timing(logpath)
+            total_time, ipopt_time, ad_time, number_of_iterations, objective_value = extract_timing(logpath)
 
         if total_time:
             content = f"{ad_time:.1f}/{ipopt_time:.1f}/{total_time:.1f}"
@@ -72,18 +77,16 @@ def main():
         df.at[case, solver] = content
 
         if total_time:
-            wide_df.at[case, f"{solver}.0"] = f"{ad_time:.1f}"
-            wide_df.at[case, f"{solver}.1"] = f"{ipopt_time:.1f}"
-            wide_df.at[case, f"{solver}.2"] = f"{total_time:.1f}"
+            wide_df.at[case, f"{solver}.0"] = f"{number_of_iterations}"
+            wide_df.at[case, f"{solver}.1"] = f"{objective_value:.3e}"
         else:
             wide_df.at[case, f"{solver}.0"] = "NA"
             wide_df.at[case, f"{solver}.1"] = "NA"
-            wide_df.at[case, f"{solver}.2"] = "NA"
 
     print(df)
-    df.to_csv("result.csv")
+    df.to_csv("result_noadnlp.csv")
 
-    wide_df.to_csv("result_wide.csv")
+    wide_df.to_csv("result_iter_obj.csv")
 
 
 if __name__ == "__main__":
--- poi.py
+++ poi.py
@@ -1,8 +1,8 @@
 import pyoptinterface as poi
-from pyoptinterface import ipopt
+from pyoptinterface import ipopt, nlfunc
 
-sin = poi.sin
-cos = poi.cos
+sin = nlfunc.sin
+cos = nlfunc.cos
 
 
 from pathlib import Path
@@ -15,8 +15,8 @@ libipopt_path = r"D:\Ipopt\bin\ipopt-3.dll"
 ipopt.load_library(libipopt_path)
 
 
-def get_ipopt_model():
-    model = ipopt.Model()
+def get_ipopt_model(jit):
+    model = ipopt.Model(jit=jit)
     model.set_raw_parameter("print_level", 5)
     model.set_raw_parameter("hsllib", "libhsl.dll")
     model.set_raw_parameter("linear_solver", "ma27")
@@ -26,7 +26,7 @@ def get_ipopt_model():
     return model
 
 
-def solve_opf(model, data, jit_engine="LLVM"):
+def solve_opf(model, data):
     Nbus = len(data["bus"])
     va = [model.add_variable() for _ in range(Nbus)]
 
@@ -56,12 +56,21 @@ def solve_opf(model, data, jit_engine="LLVM"):
     for i in data["ref_buses"]:
         model.add_linear_constraint(1.0 * va[i - 1], poi.Eq, 0.0)
 
-    def branch_flow(x, p):
-        p_from, p_to = x["p_from"], x["p_to"]
-        q_from, q_to = x["q_from"], x["q_to"]
-        vm_from, vm_to = x["vm_from"], x["vm_to"]
-        va_from, va_to = x["va_from"], x["va_to"]
-        c1, c2, c3, c4, c5, c6, c7, c8 = p
+    def branch_flow(vars, params):
+        p_from, p_to = vars.p_from, vars.p_to
+        q_from, q_to = vars.q_from, vars.q_to
+        vm_from, vm_to = vars.vm_from, vars.vm_to
+        va_from, va_to = vars.va_from, vars.va_to
+        c1, c2, c3, c4, c5, c6, c7, c8 = (
+            params.c1,
+            params.c2,
+            params.c3,
+            params.c4,
+            params.c5,
+            params.c6,
+            params.c7,
+            params.c8,
+        )
 
         va_delta = va_from - va_to
         sin_ft = sin(va_delta)
@@ -98,26 +107,14 @@ def solve_opf(model, data, jit_engine="LLVM"):
 
     branch_flow_f = model.register_function(
         branch_flow,
-        var=[
-            "p_from",
-            "q_from",
-            "p_to",
-            "q_to",
-            "vm_from",
-            "vm_to",
-            "va_from",
-            "va_to",
-        ],
-        param=8,
         name="flow",
     )
 
     branch_parameters = []
     for b in data["branch"]:
-        d = []
+        d = nlfunc.Params()
         for i in range(1, 9):
-            value = b[f"c{i}"]
-            d.append(value)
+            setattr(d, f"c{i}", b[f"c{i}"])
         branch_parameters.append(d)
 
     for b, bp in zip(data["branch"], branch_parameters):
@@ -125,19 +122,18 @@ def solve_opf(model, data, jit_engine="LLVM"):
         t_idx = b["t_idx"] - 1
         f_bus = b["f_bus"] - 1
         t_bus = b["t_bus"] - 1
-        x = [
-            p[f_idx],
-            q[f_idx],
-            p[t_idx],
-            q[t_idx],
-            vm[f_bus],
-            vm[t_bus],
-            va[f_bus],
-            va[t_bus],
-        ]
-        param = bp
+        vars = nlfunc.Vars(
+            p_from=p[f_idx],
+            q_from=q[f_idx],
+            p_to=p[t_idx],
+            q_to=q[t_idx],
+            vm_from=vm[f_bus],
+            vm_to=vm[t_bus],
+            va_from=va[f_bus],
+            va_to=va[t_bus],
+        )
         con = model.add_nl_constraint(
-            branch_flow_f, x, param, poi.Eq, [0.0, 0.0, 0.0, 0.0]
+            branch_flow_f, vars=vars, params=bp, eq=[0.0, 0.0, 0.0, 0.0]
         )
 
     angmin = data["angmin"]
@@ -177,10 +173,10 @@ def solve_opf(model, data, jit_engine="LLVM"):
         model.add_quadratic_constraint(p_balance_expr[i], poi.Eq, 0.0)
         model.add_quadratic_constraint(q_balance_expr[i], poi.Eq, 0.0)
 
-    model.optimize(jit_engine=jit_engine)
+    model.optimize()
 
 
-def solve_opf_rectangular(model, data, jit_engine="LLVM"):
+def solve_opf_rectangular(model, data):
     Nbus = len(data["bus"])
 
     vmin = data["vmin"]
@@ -215,12 +211,21 @@ def solve_opf_rectangular(model, data, jit_engine="LLVM"):
             vr[i] * vr[i] + vi[i] * vi[i], poi.In, vmin[i] * vmin[i], vmax[i] * vmax[i]
         )
 
-    def branch_flow(x, p):
-        p_fr, p_to = x["p_from"], x["p_to"]
-        q_fr, q_to = x["q_from"], x["q_to"]
-        vr_fr, vr_to = x["vr_from"], x["vr_to"]
-        vi_fr, vi_to = x["vi_from"], x["vi_to"]
-        c1, c2, c3, c4, c5, c6, c7, c8 = p
+    def branch_flow(vars, params):
+        p_fr, p_to = vars.p_from, vars.p_to
+        q_fr, q_to = vars.q_from, vars.q_to
+        vr_fr, vr_to = vars.vr_from, vars.vr_to
+        vi_fr, vi_to = vars.vi_from, vars.vi_to
+        c1, c2, c3, c4, c5, c6, c7, c8 = (
+            params.c1,
+            params.c2,
+            params.c3,
+            params.c4,
+            params.c5,
+            params.c6,
+            params.c7,
+            params.c8,
+        )
 
         vfr_sq = vr_fr * vr_fr + vi_fr * vi_fr
         vto_sq = vr_to * vr_to + vi_to * vi_to
@@ -238,26 +243,14 @@ def solve_opf_rectangular(model, data, jit_engine="LLVM"):
 
     branch_flow_f = model.register_function(
         branch_flow,
-        var=[
-            "p_from",
-            "q_from",
-            "p_to",
-            "q_to",
-            "vr_from",
-            "vr_to",
-            "vi_from",
-            "vi_to",
-        ],
-        param=8,
         name="flow",
     )
 
     branch_parameters = []
     for k, b in enumerate(data["branch"]):
-        d = []
+        d = nlfunc.Params()
         for i in range(1, 9):
-            value = b[f"c{i}"]
-            d.append(value)
+            setattr(d, f"c{i}", b[f"c{i}"])
         branch_parameters.append(d)
 
     for b, bp in zip(data["branch"], branch_parameters):
@@ -265,7 +258,7 @@ def solve_opf_rectangular(model, data, jit_engine="LLVM"):
         t_idx = b["t_idx"] - 1
         f_bus = b["f_bus"] - 1
         t_bus = b["t_bus"] - 1
-        x = [
+        vars = [
             p[f_idx],
             q[f_idx],
             p[t_idx],
@@ -275,14 +268,23 @@ def solve_opf_rectangular(model, data, jit_engine="LLVM"):
             vi[f_bus],
             vi[t_bus],
         ]
-        param = bp
+        vars = nlfunc.Vars(
+            p_from=p[f_idx],
+            q_from=q[f_idx],
+            p_to=p[t_idx],
+            q_to=q[t_idx],
+            vr_from=vr[f_bus],
+            vr_to=vr[t_bus],
+            vi_from=vi[f_bus],
+            vi_to=vi[t_bus],
+        )
         con = model.add_nl_constraint(
-            branch_flow_f, x, param, poi.Eq, [0.0, 0.0, 0.0, 0.0]
+            branch_flow_f, vars=vars, params=bp, eq=[0.0, 0.0, 0.0, 0.0]
         )
 
-    def branch_angle(x):
-        vr_fr, vr_to = x["vr_from"], x["vr_to"]
-        vi_fr, vi_to = x["vi_from"], x["vi_to"]
+    def branch_angle(vars):
+        vr_fr, vr_to = vars.vr_from, vars.vr_to
+        vi_fr, vi_to = vars.vi_from, vars.vi_to
 
         re_vfr_vto = vr_fr * vr_to + vi_fr * vi_to
         im_vfr_vto = vi_fr * vr_to - vr_fr * vi_to
@@ -293,12 +295,6 @@ def solve_opf_rectangular(model, data, jit_engine="LLVM"):
 
     branch_angle_f = model.register_function(
         branch_angle,
-        var=[
-            "vr_from",
-            "vr_to",
-            "vi_from",
-            "vi_to",
-        ],
         name="angle",
     )
     angmin = data["angmin"]
@@ -308,14 +304,17 @@ def solve_opf_rectangular(model, data, jit_engine="LLVM"):
         t_idx = b["t_idx"] - 1
         f_bus = b["f_bus"] - 1
         t_bus = b["t_bus"] - 1
-        x = [
-            vr[f_bus],
-            vr[t_bus],
-            vi[f_bus],
-            vi[t_bus],
-        ]
+        vars = nlfunc.Vars(
+            vr_from=vr[f_bus],
+            vr_to=vr[t_bus],
+            vi_from=vi[f_bus],
+            vi_to=vi[t_bus],
+        )
         con = model.add_nl_constraint(
-            branch_angle_f, x, poi.In, [math.tan(angmin[i])], [math.tan(angmax[i])]
+            branch_angle_f,
+            vars=vars,
+            lb=[math.tan(angmin[i])],
+            ub=[math.tan(angmax[i])],
         )
 
     for i, b in enumerate(data["branch"]):
@@ -352,7 +351,7 @@ def solve_opf_rectangular(model, data, jit_engine="LLVM"):
         model.add_quadratic_constraint(p_balance_expr[i], poi.Eq, 0.0)
         model.add_quadratic_constraint(q_balance_expr[i], poi.Eq, 0.0)
 
-    model.optimize(jit_engine=jit_engine)
+    model.optimize()
 
 
 def poi_main(logdir, filename, method, jit_engine="LLVM"):
@@ -360,7 +359,7 @@ def poi_main(logdir, filename, method, jit_engine="LLVM"):
     # filename = "pglib_opf_case78484_epigrids"
     # filename = "pglib_opf_case10480_goc"
     # filename = "pglib_opf_case19402_goc"
-    model = get_ipopt_model()
+    model = get_ipopt_model(jit_engine)
     logpath = str(logdir / f"{filename}_poi_{jit_engine}.log")
     model.set_raw_parameter("output_file", logpath)
 
@@ -371,9 +370,9 @@ def poi_main(logdir, filename, method, jit_engine="LLVM"):
 
     t0 = time.time()
     if method == "polar":
-        solve_opf(model, data, jit_engine=jit_engine)
+        solve_opf(model, data)
     else:
-        solve_opf_rectangular(model, data, jit_engine=jit_engine)
+        solve_opf_rectangular(model, data)
     t1 = time.time()
 
     # with open("jit.c", "w") as f:
